01dbb9601e068f864d0af119aa8ad12f85d85548
heySasha <iatsenko.o@donnu.edu.ua>
2017-07-17 23:07:24 +0300

    Add PATCH /todos/:id route

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { ObjectID } = require('mongodb');
 
 const { mongoose } = require('./db/mongoose');
 const { Todo } = require('./models/todo');
@@ -26,6 +27,24 @@ app.post('/todos', (req, res) => {
 app.get('/todos', (req, res) => {
     Todo.find().then(todos => {
         res.send({ todos });
+    }, err => {
+        return res.status(400).send(err);
+    })
+});
+
+app.patch('/todos/:id', (req, res) => {
+    const { id } = req.params;
+
+    if (!ObjectID.isValid(id)) {
+        return res.status(404).send();
+    }
+
+    Todo.findById(id).then(todo => {
+        if (!todo) {
+            return res.status(404).send();
+        }
+
+        res.send({ todo })
     }, err => {
         return res.status(400).send(err);
     })
